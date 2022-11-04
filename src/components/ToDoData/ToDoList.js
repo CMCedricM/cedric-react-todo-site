12346da6db8c8a29819577afe53fe41138682ee2
Cedric Men <cedricm13@outlook.com>
2022-11-03 17:26:34 -0700

    added better styling and added add item button

---
+++ src/components/ToDoData/ToDoList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ToDo from "../ToDoItem/ToDoItem";
+import "./ToDoList.css"
+import uuid from "react-uuid";
+const ToDoData = ({toDoItems, removeHandler, completeItemHandler}) => {
+    return(
+        <div className='todo-container'>
+            <ul className='todo-list'>
+                {toDoItems.map((data) => (<ToDo toDoText={data.itemName} toDo = {data} removeAnItemHandler={removeHandler} completeItemHandler={completeItemHandler} key={data.id ? data.id : uuid()}></ToDo>))}
+            </ul>
+        </div>
+    );
+}
+
+export default ToDoData;
